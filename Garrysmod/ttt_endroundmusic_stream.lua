fbda04ed9915618e3aeabe12d643ab089b9c7e90
TheMerkyShadow <themerkyshadows@gmail.com>
2019-07-31 20:35:08 -0400

    yes

---
+++ Garrysmod/ttt_endroundmusic_stream.lua
@@ -0,0 +1,73 @@
+sounds = {
+	innocent = {
+		"example1.mp3"
+	},
+	traitor = {
+		"example2.mp3"
+	},
+	timeout = {
+		"example3.mp3"
+	}
+}
+
+if SERVER then
+	url = ( "http://127.0.0.1/audio/" )
+	util.AddNetworkString("EndMusic")
+	hook.Add("TTTEndRound", "endroundurl", function(result)
+		local file = ""
+		if result == WIN_TRAITOR then
+			file, _ = table.Random(sounds.traitor)
+		elseif result == WIN_INNOCENT then
+			file, _ = table.Random(sounds.innocent)
+		else
+			file, _ = table.Random(sounds.timeout)	
+		end	
+		net.Start("EndMusic")
+			net.WriteString( url .. file)
+        	net.Broadcast()
+	end)
+else
+
+	CreateClientConVar( "music", "1", true, false )
+	CreateClientConVar( "music_vol", "0.75", true, false )
+	
+	hook.Add("TTTSettingsTabs", "setting", function(dtabs)
+		custom = vgui.Create( "DPanel" )	
+		list = vgui.Create( "DPanelList", custom )	
+		list:SetSpacing( 5 )		
+		list:Dock( FILL )
+		list:DockMargin( 5, 5, 5, 0 )
+		
+		local volume = vgui.Create( "DNumSlider", list )
+			volume:SetText( "Volume" )
+			volume:SetDark(true)
+			volume:SetMin( 0 )
+			volume:SetMax( 1 )
+			volume:SetDecimals( 2 )
+			volume:SetConVar( "music_vol" ) 
+				
+		local check = vgui.Create( "DCheckBoxLabel", list )
+			check:SetText( "Allow EndRound Music" )
+			check:SetDark(true)
+			check:SetConVar( "music" )
+			
+		list:AddItem( volume )
+		list:AddItem( check )		
+		
+		dtabs:AddSheet( "Custom", custom, "icon16/tick.png" )
+	end)
+   
+	net.Receive( "EndMusic", function() 
+		music = GetConVar("music"):GetInt() 
+		music_vol = GetConVar("music_vol"):GetFloat()
+		if music then
+			sound.PlayURL(net.ReadString(), "", function(station)
+				if ( IsValid( station ) ) then
+					station:Play()
+					station:SetVolume(music_vol)
+				end
+			end)
+		end
+	end)
+	
+end
---
+++ Space Engineers/StayOnPlanet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Timers;
+using Sandbox.Common.ObjectBuilders;
+using Sandbox.Definitions;
+using Sandbox.Game;
+using Sandbox.Game.WorldEnvironment;
+using Sandbox.Game.SessionComponents;
+using Sandbox.Game.Gui;
+using Sandbox.ModAPI;
+
+using VRage.ObjectBuilders;
+using VRage.Collections;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.ModAPI;
+using VRageMath;
+
+public class Merky : MySessionComponentBase
+{
+
+    public Merky()
+    {
+        var timer = new System.Timers.Timer();
+        timer.Elapsed += new ElapsedEventHandler(this.Think);
+        timer.Interval = 1000; // 1 second
+        timer.Enabled = true;
+
+    }
+
+    public void Think(object sender, ElapsedEventArgs args)
+    {
+
+        var SpawnPlanet = MyAPIGateway.Session.GetWorld().Planets[0];
+        var pos = SpawnPlanet.PositionAndOrientation.Value.Position;
+        
+        var players = new List<IMyPlayer>();
+        MyAPIGateway.Players.GetPlayers(players);
+
+        foreach (var ply in players)
+        {
+
+            var identify = ply.Identity;
+            var character = ply.Character;
+
+            if (!character.IsDead)
+            {
+
+                var max = (SpawnPlanet.Radius + SpawnPlanet.AtmosphereRadius);
+                var dist = Vector3D.Distance(pos, ply.GetPosition());
+
+                if (dist >= max) { character.Kill(); }
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
