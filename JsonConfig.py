3e73eab4932ab44fb2b3957648d15da028581634
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2020-07-21 21:53:49 -0400

    Create JsonConfig.py
    JsonConfig

---
+++ JsonConfig.py
@@ -0,0 +1,17 @@
+class JsonConfig:
+    def __init__(self, dir='manifest.json'):
+        self.Path = Path(dir)
+        self.Reload()
+    def Reload(self):
+        self.Table = self.Decode()
+    def Decode(self):
+        return json.loads(self.Path.read_text())
+    def Encode(self):
+        return json.dumps((self.Table), indent=4)
+    def __getitem__(self, item):
+        return self.Decode()[item]
+    def __setitem__(self, item, value):
+        self.Table[item] = value
+        self.Write()
+    def Write(self):
+        self.Path.write_text(self.Encode())
