5904b6853e20e33a7dcefd404640786317ffa9ef
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2020-08-04 03:38:24 -0400

    Update JsonConfig.py

--- Python/JsonConfig.py
+++ Python/JsonConfig.py
@@ -8,15 +8,14 @@ from pathlib import Path
 class JsonConfig:
     def __init__(self, dir='manifest.json'):
         self.Path = Path(dir)
-        self.Reload()
-    def Reload(self):
-        self.Table = self.Decode()
+        self.Decode()
     def Decode(self):
-        return json.loads(self.Path.read_text())
+        self.Table = json.loads(self.Path.read_text())
+        return self.Table
     def Encode(self):
-        return json.dumps((self.Table), indent=4)
+        return json.dumps(self.Table, indent=4)
     def __getitem__(self, item):
-        return self.Decode()[item]
+        return self.Table[item]
     def __setitem__(self, item, value):
         self.Table[item] = value
         self.Write()
