a56bc416002eed20acf802d6b4efbf1c2891394c
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2022-08-29 02:41:55 -0400

    Create filechanger.py

---
+++ Python/filechanger.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time
+
+from pathlib import Path
+
+from tkinter import *
+from tkinter.filedialog import askopenfilenames
+
+files = askopenfilenames(initialdir=os.getcwd())
+if files:
+    for filename in files:
+        file = Path(filename)
+
+        # Apply Rule Here
+        rule = filename.lower()
+
+        file.rename( rule )
+        exit()
