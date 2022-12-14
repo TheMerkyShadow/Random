43566cd9fb41f788c4bfd5bdafa6444b0762368c
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2022-12-13 22:31:42 -0500

    ChatGPT

--- Python/JsonConfig.py
+++ Python/JsonConfig.py
@@ -6,28 +6,19 @@ import json
 from pathlib import Path
 
 class JsonConfig:
-    def __init__(self, dir='manifest.json'):
+    def __init__(self, dir='/'):
         self.Path = Path(dir)
-        self.Check()
         self.Decode()
-    def Check(self):
-        if not self.Path.exists():
-            self.Path.write_text("{}")
     def Decode(self):
         self.Table = json.loads(self.Path.read_text())
-        return self.Table
     def Encode(self):
         return json.dumps(self.Table, indent=4)
     def __getitem__(self, item):
-        try:
-            return self.Table[item]
-        except Exception as e:
-            return None
+        return self.Table.get(item, None)
     def __setitem__(self, item, value):
-        self.Table[item] = value
-        self.Write()
-    def __delitem__( self, value ):
-        self.Table.pop(value)
-        self.Write()  
+        if item in self.Table:
+            self.Table[item] = value
     def Write(self):
         self.Path.write_text(self.Encode())
+    def __repr__(self) -> str:
+        return repr(self.Table)
