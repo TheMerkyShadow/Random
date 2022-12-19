ccfa41bb62a657b4d170cd2eeda35ac4385c1f6e
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2022-12-19 13:40:11 -0500

    Update and rename JsonConfig.py to JsonDictConfig.py

--- Python/JsonDictConfig.py
+++ Python/JsonDictConfig.py
@@ -5,19 +5,21 @@ import json
 
 from pathlib import Path
 
-class JsonConfig:
-    def __init__(self, dir='/'):
+class JsonDictConfig:
+    def __init__(self, dir='config.json'):
         self.Path = Path(dir)
-        self.Decode()
-    def Decode(self):
+        self.Read() 
+    def Read(self):
         self.Table = json.loads(self.Path.read_text())
     def Encode(self):
         return json.dumps(self.Table, indent=4)
     def __getitem__(self, item):
         return self.Table.get(item, None)
-    def __setitem__(self, item, value):
-        if item in self.Table:
-            self.Table[item] = value
+    def __setitem__(self, item, value)
+        self.Table.setdefault(item, value)    
+    def __delitem__(self, item):
+         if item in self.Table:
+              del self.Table[item]        
     def Write(self):
         self.Path.write_text(self.Encode())
     def __repr__(self) -> str:
