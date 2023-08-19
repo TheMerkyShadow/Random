4c07b614f5ccb0c2e9bbd7ae2ef0686f6056ac81
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2023-08-19 09:57:28 -0400

    Update gap.py

--- Work/gap.py
+++ Work/gap.py
@@ -1,25 +1,27 @@
 import datetime
-import math
 
-def calculate():
-    goal = int(input("Sales Goal: "))
-    current_sales = int(input("Current Sales: "))
-
+def calculate_sales_goal():
+    goal = int(input("Enter the sales goal: "))
+    current_sales = int(input("Enter the current sales: "))
+    
     current_date = datetime.datetime.now()
     last_day = (current_date.replace(day=28) + datetime.timedelta(days=4)).replace(day=1) - datetime.timedelta(days=1)
     
     remaining_days = 0
     days_worked = 0
+
     for day in range(current_date.day, last_day.day + 1):
-        if current_date.replace(day=day).weekday() != 6:  # Sunday is 6
-            if (day - current_date.day) % 7 != 0:
-                remaining_days += 1
-                days_worked += 1
+        if current_date.replace(day=day).weekday() != 6:
+            remaining_days += 1
+            days_worked += 1
     
     gap = abs((current_sales - goal) / days_worked)
-    rounded_gap = math.ceil(gap) if gap > 1 else (0 if gap < 1 else gap)
+    boxes = int(gap) if gap >= 1 else 0
     
     print(f"Remaining days (excluding Sundays and off days): {remaining_days}")
-    print(f"Gap: {rounded_gap:.1f}")
+    print(f"Gap: {gap:.2f}")
+    print(f"Boxes: {boxes:.1f}")
 
-calculate()
+if __name__ == "__main__":
+    print("Sales Goal Calculator")
+    calculate_sales_goal()
