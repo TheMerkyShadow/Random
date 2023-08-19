0a7ea24ef2e33be26827b8445b003477390e5d60
TheMerkyShadow <32728280+TheMerkyShadow@users.noreply.github.com>
2023-08-19 09:23:28 -0400

    Update gap.py

--- Work/gap.py
+++ Work/gap.py
@@ -1,27 +1,23 @@
-from datetime import datetime, timedelta
-import calendar
+import datetime
 
-# Input your sales goal and current sales
-goal = int(input("Enter your sales goal: "))
-current_sales = int(input("Enter your current sales: "))
+def calculate():
+    goal = int(input("Sales Goal: "))
+    current_sales = int(input("Current Sales: "))
 
-# Get the current date
-current_date = datetime.now()
+    current_date = datetime.datetime.now()
+    last_day = (current_date.replace(day=28) + datetime.timedelta(days=4)).replace(day=1) - datetime.timedelta(days=1)
+    
+    remaining_days = 0
+    days_worked = 0
+    for day in range(current_date.day, last_day.day + 1):
+        if current_date.replace(day=day).weekday() != 6:  # Sunday is 6
+            if (day - current_date.day) % 7 != 0:
+                remaining_days += 1
+                days_worked += 1
+    
+    gap = abs((current_sales - goal) / days_worked)
+    
+    print(f"Remaining days (excluding Sundays and off days): {remaining_days}")
+    print(f"Gap: {gap:.2f}")
 
-# Get the last day of the current month
-_, last_day = calendar.monthrange(current_date.year, current_date.month)
-
-# Calculate the remaining days while excluding Sundays and off days
-remaining_days = 0
-days_worked = 0
-for day in range(current_date.day, last_day + 1):
-    if datetime(current_date.year, current_date.month, day).weekday() != 6:  # Sunday
-        if (day - current_date.day) % 7 != 0:  # Exclude off days every 7 days
-            remaining_days += 1
-            days_worked += 1
-
-# Calculate the positive gap
-positive_gap = (goal - current_sales) / days_worked
-
-print("Remaining days (excluding Sundays and off days):", remaining_days)
-print("Positive Gap:", positive_gap)
+calculate()
